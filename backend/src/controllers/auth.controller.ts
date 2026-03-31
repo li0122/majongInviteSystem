b9838dfd8dbfe88b84ff6327047c99b6196f98f7
Pineapple0122 <li0122.lchs@gmail.com>
2026-03-31 18:37:10 +0800

    fix: replace bcrypt with bcryptjs in dependencies and imports

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { UserModel } from "../models/User";
 import { generateOtp, sendOtpEmail } from "../services/otp.service";
